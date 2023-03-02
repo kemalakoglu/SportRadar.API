85ecb7538839b527582d0ad0b8029e0d0e963293
kemalakoglu <kemalakoglu@outlook.com>
2023-03-03 00:31:35 +0100

    test scenarios were added

---
+++ src/02-Domain/SportRadar.Domain.Test/SportRadar.Domain.Test/FixTureQueryHandlerTest.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+namespace SportRadar.Domain.Test
+{
+    public class FixTureQueryHandlerTest
+    {
+        [Fact]
+        public void ShouldStartEventsQueryHandlerReturnMatchList()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldStartEventsQueryHandlerReturnEmpty()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldStartEventsQueryHandlerThrowsError()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldFinishEventsQueryHandlerReturnMatchListResult()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldFinishEventsQueryHandlerReturnNoMatchWarning()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldFinishEventsQueryHandlerThrowsError()
+        {
+
+        }
+    }
+}
---
+++ src/03-Application/SportRadar.Application.Test/SportRadar.Application.Test/FixTureQueryTest.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+namespace SportRadar.Application.Test
+{
+    public class FixTureQueryTest
+    {
+        [Fact]
+        public void ShouldStartEventsQueryReturnMatchList()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldStartEventsQueryReturnEmpty()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldStartEventsQueryThrowsError()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldFinishEventsQueryReturnMatchListResult()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldFinishEventsQueryReturnNoMatchWarning()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldFinishEventsQueryThrowsError()
+        {
+
+        }
+    }
+}
